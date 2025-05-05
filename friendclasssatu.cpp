9e93e9c6e46383cce08dbf16dcdaed30bd44c001
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-05 15:54:32 +0700

    menambahkan

---
+++ friendclasssatu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+string nama;
+
+public :
+void setnama(string pnama);
+friend class siswa;
+};
+
+
